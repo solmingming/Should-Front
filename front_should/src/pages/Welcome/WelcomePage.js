d748c568890c854a9606925bb588bd43675326f5
solmingming <leesm0404a@ewhain.net>
2023-08-27 17:29:05 +0900

    change design

--- front_should/src/pages/Welcome/WelcomePage.js
+++ front_should/src/pages/Welcome/WelcomePage.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './WelcomePage.css'; // You can style your page using CSS
 
-function WelcomePage() {
+const WelcomePage = () => {
   return (
-    <div className="welcome-container">
-      <h1>Welcome to My React Welcome Page!</h1>
-      <p>This is a simple welcome page built using React.</p>
+    <div className="welcome-page">
+      <h1 className="main-title">CHAT.DA</h1>
+      <div className="button-container">
+        <div className="button-row">
+          <button className="sign-in-button">SIGN IN</button>
+        </div>
+        <div className="button-row">
+          <button className="sign-up-button">SIGN UP</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default WelcomePage;
