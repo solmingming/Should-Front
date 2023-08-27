8221b108b32133529155515f3c98f69c589384ce
solmingming <leesm0404a@ewhain.net>
2023-08-27 17:25:21 +0900

    add welcome page

--- front_should/src/App.js
+++ front_should/src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/User/HomePage";
 import OnChat from "./pages/Chat/OnChat";
 import TaskPage from "./pages/Task/TaskPage";
+import WelcomePage from "./pages/Welcome/WelcomePage";
 
 function App() {
     return (
-        <>
+    
             <Router>
                 <Routes>
                     <Route path="/homepage" element={<HomePage />} />
                     <Route path="/onchat" element={<OnChat />} />
                     <Route path="/taskpage" element={<TaskPage />} />
+                    <Route path="/" element={<WelcomePage />} />
                 </Routes>
             </Router>
-        </>
+        
     );
 }
 
---
+++ front_should/src/pages/Welcome/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WelcomePage() {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to My React Welcome Page!</h1>
+      <p>This is a simple welcome page built using React.</p>
+    </div>
+  );
+}
+
+export default WelcomePage;
