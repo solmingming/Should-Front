07a3ec3a72a02bb595d11f2af67342296756e5ea
JiwonChoi0805 <won0805@ewhain.net>
2023-09-02 12:54:17 +0900

    달력-태스크 연결

--- front_should/src/pages/Calendar/Calendar.js
+++ front_should/src/pages/Calendar/Calendar.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';  
-import { useParams } from 'react-router-dom'; 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
 import Header from "../../components/Header";
-import moment from 'moment';
-import './Calendar.css';
+import moment from "moment";
+import "./Calendar.css";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,85 +13,88 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Calendar = () => {
+    const { date } = useParams();
+    const [selectedDate, setSelectedDate] = useState(moment());
 
-  const { date } = useParams();
-  const [selectedDate, setSelectedDate] = useState(moment());
+    const renderCalendar = () => {
+        const monthStart = selectedDate.clone().startOf("month");
+        const monthEnd = selectedDate.clone().endOf("month");
+        const startDate = monthStart.clone().startOf("week");
+        const endDate = monthEnd.clone().endOf("week");
+        const calendar = [];
+        let week = [];
 
-  
-  const renderCalendar = () => {
-    const monthStart = selectedDate.clone().startOf('month');
-    const monthEnd = selectedDate.clone().endOf('month');
-    const startDate = monthStart.clone().startOf('week');
-    const endDate = monthEnd.clone().endOf('week');
-    const calendar = [];
-    let week = [];
+        let day = startDate.clone();
+        while (day.isSameOrBefore(endDate, "day")) {
+            week.push(
+                <div key={day.format("YYYY-MM-DD")}>
+                    <Link
+                        to={`/date/${day.format("YYYY-MM-DD")}`}
+                        className="calendar-day-link"
+                        style={{ textDecoration: "none" }}
+                    >
+                        {day.format("D")}
+                    </Link>
+                </div>
+            );
 
-    let day = startDate.clone();
-    while (day.isSameOrBefore(endDate, 'day')) {
-      week.push(
-        <div key={day.format('YYYY-MM-DD')}>
-        <Link to={`/date/${day.format('YYYY-MM-DD')}`} className="calendar-day-link" style={{ textDecoration: 'none' }}> 
-        {day.format('D')}
-        </Link>
-        </div>
+            if (day.day() === 6) {
+                calendar.push(
+                    <div
+                        key={day.format("YYYY-MM-DD")}
+                        className="calendar-week"
+                    >
+                        {week}
+                    </div>
+                );
+                week = [];
+            }
+
+            day.add(1, "day");
+        }
+
+        return calendar;
+    };
+
+    const prevMonth = () => {
+        setSelectedDate(selectedDate.clone().subtract(1, "month"));
+    };
+
+    const nextMonth = () => {
+        setSelectedDate(selectedDate.clone().add(1, "month"));
+    };
+
+    return (
+        <Wrapper>
+            <GlobalStyle />
+            <Header title="calendar" />
+            <Background>
+                <div className="calendar-container">
+                    <div className="calendar-footer">
+                        <button className="allmybutton">All my</button>
+                    </div>
+                    <header className="calendar-header">
+                        <button onClick={prevMonth}>Prev</button>
+                        <h1>{selectedDate.format("MMMM YYYY")}</h1>
+                        <button onClick={nextMonth}>Next</button>
+                    </header>
+
+                    <div className="calendar">
+                        <div className="calendar-days-header">
+                            <div>SUN</div>
+                            <div>MON</div>
+                            <div>TUE</div>
+                            <div>WED</div>
+                            <div>THU</div>
+                            <div>FRI</div>
+                            <div>SAT</div>
+                        </div>
+                        <div className="calendar-body">{renderCalendar()}</div>
+                    </div>
+                </div>
+            </Background>
+        </Wrapper>
     );
-        
-    if (day.day() === 6) {
-      calendar.push(
-        <div key={day.format('YYYY-MM-DD')} className="calendar-week">
-          {week}
-        </div>
-      );
-      week = [];
-    }
-
-    day.add(1, 'day');
-  }
-
-    return calendar;
-  };
-
-  const prevMonth = () => {
-    setSelectedDate(selectedDate.clone().subtract(1, 'month'));
-  };
-
-  const nextMonth = () => {
-    setSelectedDate(selectedDate.clone().add(1, 'month'));
-  };
-
-  return (
-    <Wrapper>
-      <GlobalStyle />
-      <Header title="calendar" />
-      <Background>
-
-        <div className="calendar-container">
-        <div className="calendar-footer">
-            <button className="allmybutton">All my</button>
-          </div>
-        <header className="calendar-header">
-            <button onClick={prevMonth}>Prev</button>
-            <h1>{selectedDate.format('MMMM YYYY')}</h1>
-            <button onClick={nextMonth}>Next</button>
-          </header>
-          
-          <div className="calendar">
-            <div className="calendar-days-header">
-              <div>SUN</div>
-              <div>MON</div>
-              <div>TUE</div>
-              <div>WED</div>
-              <div>THU</div>
-              <div>FRI</div>
-              <div>SAT</div>
-            </div>
-            <div className="calendar-body">{renderCalendar()}</div>
-          </div>
-          
-        </div>
-      </Background>
-    </Wrapper>
-  );
 };
 
 export default Calendar;
